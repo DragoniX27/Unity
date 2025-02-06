b3082637554d8593eb05c69c5dd4741c9fd5b6c6
Santiago Giraldo <santaigogil271@gmail.com>
2025-02-06 12:02:45 -0500

    agregando funcionalidad de salto y animaciones

--- Assets/Scripts/EnchantressMovement.cs
+++ Assets/Scripts/EnchantressMovement.cs
@@ -8,12 +8,14 @@ public class EnchantressMovement : MonoBehaviour
     public float Speed;
     public float JumpForce;
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
     private bool Grounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,15 +23,22 @@ public class EnchantressMovement : MonoBehaviour
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
 
+        if(Horizontal < 0.0f) transform.localScale = new Vector3(-0.4f,0.4f,0.4f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.4f,0.4f,0.4f);
+
+        Animator.SetBool("Running", Horizontal != 0.0f);
+
         Debug.DrawRay(transform.position, Vector3.down*0.14f, Color.red);
 
-        if(Physics2D.Raycast(transform.position, Vector3.down, 0.14f)){
-            Grounded  = true;
-        }else{
-            Grounded = false;
-        }
+    
+        Grounded  = Physics2D.Raycast(transform.position, Vector3.down, 0.14f);
+        Animator.SetBool("Grounded", Grounded);
+
         if(Input.GetKeyDown(KeyCode.Space) && Grounded){
+            Animator.SetBool("Jumping", true);
             Jump();
+        }else{
+            Animator.SetBool("Jumping", false);
         }
     }
 
